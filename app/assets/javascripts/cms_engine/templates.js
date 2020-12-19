c2d65606cf98c49f9a89a43f982a2bb77a06a91c
mdziardziel <mdziardziel@gmail.com>
2020-12-19 16:23:06 +0100

    remove dynamically elements

--- app/assets/javascripts/cms_engine/templates.js
+++ app/assets/javascripts/cms_engine/templates.js
@@ -1,13 +1,29 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+const removeElement = (self) => {
+  $(self).closest('.element-row').remove();
+}
+
+const addElement = () => {
+  const lastElement = $("#elements-card").find('.element-row:last');
+  const newElement = lastElement.clone();
+  newElement.find('input').val("");
+  newElement.find('select').val("");
+  lastElement.after(newElement);
+}
+
 $(window).on('load', function() {
   $("#add-element").click(function() {
-    const lastElement = $("#elements-card").find('.input-group:last');
-    const newElement = lastElement.clone();
-    newElement.find('input').val("");
-    newElement.find('select').val("");
-    lastElement.after(newElement);
+    addElement();
+
+    $(".remove-element").click(function() {
+      removeElement(this);
+    });
+  });
+
+  $(".remove-element").click(function() {
+    removeElement(this);
   });
 });
 
