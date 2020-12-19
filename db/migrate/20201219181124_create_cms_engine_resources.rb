6da45f171fec1a85f685e1eb9534f3a5286c3ac3
mdziardziel <mdziardziel@gmail.com>
2020-12-19 19:53:00 +0100

    fix migration

--- db/migrate/20201219181124_create_cms_engine_resources.rb
+++ db/migrate/20201219181124_create_cms_engine_resources.rb
@@ -1,11 +1,11 @@
 class CreateCmsEngineResources < ActiveRecord::Migration[5.2]
   def change
     create_table :cms_engine_resources do |t|
-      t.references :template, foreign_key: true
+      t.references :cms_engine_template, foreign_key: true
       t.string :name
       t.string :path
-      t.boolean :published
-      t.jsonb :elements
+      t.boolean :published, default: false
+      t.jsonb :elements, null: false, default: '[]'
       t.string :language
 
       t.timestamps
