d0c5cdc26a22ef90f2088f89b336ee64bf7a509d
mdziardziel <mdziardziel@gmail.com>
2020-12-23 18:10:33 +0100

    helper selects input path if exists or fallback

--- app/helpers/cms_engine/resources_helper.rb
+++ app/helpers/cms_engine/resources_helper.rb
@@ -1,4 +1,19 @@
 module CmsEngine
   module ResourcesHelper
+    def render_input(input_type, default_value, form)
+      input_path = "cms_engine/resources/inputs/#{input_type}"
+      fallback_path = 'cms_engine/resources/inputs/string'
+
+      partial_path = partial_or_fallback_path(input_path, fallback_path)
+      render(partial_path, default_value: default_value, form: form)
+    end
+
+    private
+    
+    def partial_or_fallback_path(path, fallback_path)
+      return path if lookup_context.template_exists?(path, [], true)
+
+      fallback_path
+    end
   end
 end
