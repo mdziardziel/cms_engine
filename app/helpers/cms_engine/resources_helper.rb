d80ddfea4c9e73723daa9c0041ecde19dee8c388
mdziardziel <mdziardziel@gmail.com>
2021-01-02 16:09:54 +0100

    hack html input

--- app/helpers/cms_engine/resources_helper.rb
+++ app/helpers/cms_engine/resources_helper.rb
@@ -1,11 +1,12 @@
 module CmsEngine
   module ResourcesHelper
-    def render_input(input_type, form, options = {})
-      input_path = "cms_engine/resources/inputs/#{input_type}"
-      fallback_path = 'cms_engine/resources/inputs/string'
+    def render_input(input_type, form, key, options = {})
+      # TODO fix html
+      input_path = "cms_engine/resources/inputs/#{input_type.to_s == 'html' ? 'text' : input_type}_input"
+      fallback_path = 'cms_engine/resources/inputs/string_input'
 
       partial_path = partial_or_fallback_path(input_path, fallback_path)
-      render(partial_path, form: form, options: options)
+      render(partial_path, form: form, options: options, key: key)
     end
 
     private
